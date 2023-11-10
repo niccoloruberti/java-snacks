c89301c24abdf87e147559aabe01954151113bc1
niccoloruberti <nikiruberti@gmail.com>
2023-11-10 11:40:46 +0100

    snack1

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,9 +1,26 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		//chiedo un numero all'utente
+		System.out.println("Inserisci un numero:");
+		String numStr = in.nextLine();
+		int num = Integer.valueOf(numStr);
+		
+		in.close();
+		
+		//se il numero è pari lo stampo altirmenti stampo il successivo
+		if(num % 2 == 0) {
+			System.out.println(num);
+		} else {
+			System.out.println(num + 1);
+		}
 
 	}
 
