e0248826fd5b52fa53709efa7903e25eac8d12f6
niccoloruberti <nikiruberti@gmail.com>
2023-11-10 12:09:09 +0100

    snack3

---
+++ src/org/lessons/java/Snack3.java
@@ -0,0 +1,23 @@
+package org.lessons.java;
+
+public class Snack3 {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+		
+		int sum = 0;
+		
+		for(int x = 0; x < numbers.length; x++) {
+			
+			if(x % 2 == 0) {
+				sum += numbers[x];
+			}
+			
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
